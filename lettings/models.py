f60cf7d082f1e6865171d07af58341450c06d1a9
Mathis Chatelain <mathis.chatelain@socotec.com>
2023-10-26 14:51:05 +0200

    finish moving models from old app + fix migrations

--- lettings/models.py
+++ lettings/models.py
@@ -1,19 +1,45 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinLengthValidator
 
+
 class Address(models.Model):
+    """
+    Address model
+
+    uses number and street as representation
+    """
+
+    class Meta:
+        """
+        NOTE : this Model was originially created in the oc_lettings_site app
+        and then moved to lettings app since it is related to the Letting model
+        """
+
+        db_table = "oc_lettings_site_address"
+
     number = models.PositiveIntegerField(validators=[MaxValueValidator(9999)])
     street = models.CharField(max_length=64)
     city = models.CharField(max_length=64)
     state = models.CharField(max_length=2, validators=[MinLengthValidator(2)])
     zip_code = models.PositiveIntegerField(validators=[MaxValueValidator(99999)])
-    country_iso_code = models.CharField(max_length=3, validators=[MinLengthValidator(3)])
+    country_iso_code = models.CharField(
+        max_length=3, validators=[MinLengthValidator(3)]
+    )
 
     def __str__(self):
-        return f'{self.number} {self.street}'
+        return f"{self.number} {self.street}"
 
 
 class Letting(models.Model):
+    """Letting model"""
+
+    class Meta:
+        """
+        NOTE : this Model was originially created in the oc_lettings_site app
+        """
+
+        db_table = "oc_lettings_site_letting"
+
     title = models.CharField(max_length=256)
     address = models.OneToOneField(Address, on_delete=models.CASCADE)
 
--- profiles/models.py
+++ profiles/models.py
@@ -1,7 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Profile(models.Model):
+    """
+    Profile model
+    associated with a User model instance
+
+    NOTE : this Model was originially created in the oc_lettings_site app
+    and then moved to its own "profiles" app
+    """
+
+    class Meta:
+        db_table = "oc_lettings_site_profile"
+
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     favorite_city = models.CharField(max_length=64, blank=True)
 
