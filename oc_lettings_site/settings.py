25ceecccded811f81d4b9134ffad306b577cc879
Mathis Chatelain <mathis.chatelain@socotec.com>
2023-11-04 00:56:33 +0100

    uncompress whitenoise

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -108,7 +108,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.StaticFilesStorage"
 
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [
