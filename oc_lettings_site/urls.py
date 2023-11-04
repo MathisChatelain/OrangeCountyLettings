e45cdbb4eae1a70da5dc45581d5520630b2563d5
Mathis Chatelain <mathis.chatelain@socotec.com>
2023-11-04 10:20:31 +0100

    sentry pls work

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -3,10 +3,16 @@ from django.urls import path
 
 from . import views
 
+
+def trigger_error(request):
+    division_by_zero = 1 / 0
+
+
 urlpatterns = [
     path("", views.index, name="index"),
     path("lettings/", views.lettings_index, name="lettings_index"),
     path("lettings/<int:letting_id>/", views.letting, name="letting"),
+    path("sentry-debug/", trigger_error),
     path("profiles/", views.profiles_index, name="profiles_index"),
     path("profiles/<str:username>/", views.profile, name="profile"),
     path("admin/", admin.site.urls),
--- settings.py
+++
@@ -1,6 +0,0 @@
-import sentry_sdk
-
-sentry_sdk.init(
-    dsn="https://98c783f8159e4964f864f2ddfeb24129@o4505963636391936.ingest.sentry.io/4505963639144448",
-    enable_tracing=True,
-)
