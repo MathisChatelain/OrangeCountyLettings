838db5e944db38e90a4c6e843dbc62c115ecd35c
Mathis Chatelain <mathis.chatelain@socotec.com>
2023-11-04 01:46:42 +0100

    raf

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False  # in order to show 404
+DEBUG = True  # in order to show 404
 
 ALLOWED_HOSTS = ["*"]
 
